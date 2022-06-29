f65a02025214cd8f363a103a4e9f20570fb3303d
Uamixerz <Uamixerz@gmail.com>
2022-06-29 17:15:25 +0300

    FishStart

---
+++ Fish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Fishs
+{
+	public Fish()
+	{
+		string Type { get; set; }
+
+		public Fish()
+        {
+			Random rnd = new Random();
+			Type = "s";
+        }
+
+	}
+}
