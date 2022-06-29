0b5514f504121cbcfed47932dc7e13acefe05a66
Uamixerz <Uamixerz@gmail.com>
2022-06-29 17:20:10 +0300

    FishRemake

--- Fish.cs
+++ Fish.cs
@@ -2,15 +2,47 @@
 
 public class Fishs
 {
-	public Fish()
-	{
 		string Type { get; set; }
 
 		public Fish()
         {
 			Random rnd = new Random();
-			Type = "s";
+			int x = rnd.Next(0, 101);
+
+			if (x < 40)
+				Type = "s";
+			else if (x < 75)
+				Type = "m";
+			else 
+				Type = "b";
+
+        }
+		public Eat(string TypeEat)
+        {
+			
         }
 
-	}
+		private EatS(string TypeEat)
+        {
+
+        }
+
+		private Eatm(string TypeEat)
+        {
+
+        }
+		private EatM(string TypeEat)
+		{
+
+		}
+		private Eatb(string TypeEat)
+		{
+
+		}
+		private EatB(string TypeEat)
+		{
+
+		}
+
+	
 }
